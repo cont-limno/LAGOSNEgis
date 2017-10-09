718bea087183bdd6ff8fa7887dc557cb946a7141
jsta <stachel2@msu.edu>
2017-10-09 10:53:57 -0400

    add wbd query fxn

--- R/query.R
+++ R/query.R
@@ -24,3 +24,24 @@ query_gis <- function(gis_path = NA, query, crs){
 
   sf::st_zm(dat)
 }
+
+#' Query watershed boundary for LAGOS lakes
+#'
+#' @importFrom sf st_union
+#' @export
+#' @examples \dontrun{
+#' res <- query_wbd(lagoslakeid = 7429)
+#' }
+query_wbd <- function(lagoslakeid, crs = NA){
+  if(is.na(crs)){
+    crs <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"
+  }
+
+  iws <- query_gis(query = paste0("SELECT * FROM IWS WHERE lagoslakeid=",
+                                  lagoslakeid), crs = crs)
+  lake_boundary <- query_gis(
+    query = paste0("SELECT * FROM LAGOS_NE_All_Lakes_4ha WHERE lagoslakeid=",
+                   lagoslakeid), crs = crs)
+
+  sf::st_union(st_geometry(iws), st_geometry(lake_boundary))
+}
