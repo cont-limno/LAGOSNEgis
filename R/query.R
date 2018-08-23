d0d76f396559590822666af24ad5fc86664499c2
jsta <stachel2@msu.edu>
2018-08-23 11:02:01 -0400

    order results by query id order

--- R/query.R
+++ R/query.R
@@ -11,20 +11,30 @@
 #' @examples \dontrun{
 #'
 #' library(sf)
-#' library(gdalUtils)
 #' st_layers(gis_path_default())
+#'
+#' library(gdalUtils)
 #' ogrinfo(gis_path_default(), "HU12", so = TRUE)
+#'
+#' library(mapview)
+#' res_iws <- query_gis("IWS", "lagoslakeid", c(34352))
+#' res_lake <- query_gis("LAGOS_NE_All_Lakes_4ha", "lagoslakeid", 34352)
+#' mapview(res_iws) + mapview(res_lake)
 #'
 #' res <- query_gis("IWS", "lagoslakeid", c(7010))
 #' res <- query_gis("HU12", "ZoneID", c("HU12_1"))
 #' }
 query_gis <- function(layer, id_name, ids, crs = albers_conic(),
                       gis_path = gis_path_default()){
-  query_gis_(gis_path,
+  res <- query_gis_(gis_path,
              query = paste0("SELECT * FROM ", layer,
                             " WHERE ", id_name, " IN ('",
                             paste0(ids,
                                    collapse = "', '"), "');"), crs = crs)
+
+  # sort items by ids
+  res <- res[match(ids, data.frame(res[,id_name])[,id_name]),]
+  res
 }
 
 #' Raw (non-vectorized) query of LAGOS GIS
@@ -70,30 +80,27 @@ query_gis_ <- function(gis_path, query, crs){
 #' @examples \dontrun{
 #' library(mapview)
 #' res <- query_wbd(lagoslakeid = c(7010))
+#' res <- query_wbd(lagoslakeid = c(7010, 34352))
+#' res <- query_wbd(lagoslakeid = c(34352))
 #' mapview(res)
 #'
-#' res <- query_wbd(lagoslakeid = c(2057, 3866, 1500, 3386, 2226, 1637, 6874, 7032, 1935, 6970, 5331))
+#' res <- query_wbd(lagoslakeid = c(2057, 3866, 1500, 3386, 2226, 1637, 6874, 7032, 1935, 6970, 5331, 34352))
+#' res <- res[res$lagoslakeid == 34352,]
+#' mapview::mapview(res)
 #'
 #' }
 query_wbd <- function(lagoslakeid, gis_path = gis_path_default(),
-                      crs = albers_conic(), utm = TRUE){
+                      crs = albers_conic(), utm = FALSE){
 
-  iws <- query_gis_(gis_path,
-    query = paste0("SELECT * FROM IWS WHERE lagoslakeid IN ('",
-                   paste0(lagoslakeid,
-                          collapse = "', '"), "');"), crs = crs)
+  iws           <- query_gis("IWS", "lagoslakeid", lagoslakeid)
+  lake_boundary <- query_gis("LAGOS_NE_All_Lakes_4ha", "lagoslakeid", lagoslakeid)
 
-  lake_boundary <- query_gis_(gis_path,
-    query = paste0("SELECT * FROM LAGOS_NE_All_Lakes_4ha WHERE lagoslakeid IN ('",
-                   paste0(lagoslakeid,
-                          collapse = "', '"), "');"), crs = crs)
+  res <- lapply(seq_len(nrow(iws)), function(x) {
+          iws_dissolve  <- st_buffer(do.call(c, st_geometry(iws)[x]), 0)
+          lake_dissolve <- st_buffer(do.call(c, st_geometry(lake_boundary)[x]), 0.001)
 
-    res <- lapply(seq_len(nrow(iws)), function(x) {
-    iws_dissolve  <- st_buffer(do.call(c, st_geometry(iws)[x]), 0)
-    lake_dissolve <- st_buffer(do.call(c, st_geometry(lake_boundary)[x]), 0.001)
-
-    st_geometry(st_union(iws_dissolve, lake_dissolve))
-    })
+          st_geometry(st_union(iws_dissolve, lake_dissolve))
+          })
 
   res <- do.call(c, res)
   st_crs(res) <- crs
